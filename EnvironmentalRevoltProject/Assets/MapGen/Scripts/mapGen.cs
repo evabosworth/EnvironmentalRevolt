46fcfded81d6465e42e67d6d636d7a6c5d066a0e
Evelyn Bosworth <Evelyn Bosworth>
2017-01-23 10:23:06 -0800

    added basic map gen script

---
+++ EnvironmentalRevoltProject/Assets/MapGen/Scripts/mapGen.cs
@@ -0,0 +1,30 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class mapGen : MonoBehaviour {
+	public GameObject terrain;
+	private int xSize = 30;
+	private int zSize = 30;
+
+	// Use this for initialization
+	void Start () {
+		int y = 0;
+		for (int x = 0; x < xSize; x++) {
+			for (int z = 0; z < zSize; z++) {
+				createTerrain (terrain, new Vector3 (x, y, z), x, z);
+				
+			}
+		}
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+
+	private void createTerrain(GameObject terrain, Vector3 position, int x, int z){
+		GameObject terrainClone = (GameObject)Instantiate (terrain, position, transform.rotation);
+		terrainClone.name = "terrain:x," + x + ":z," + z; 
+	}
+}
